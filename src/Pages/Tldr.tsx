f09fc27786ae097902d76ec7f1cf5e4abf5d9f12
adhipk <adhip.kashyap@gmail.com>
2024-07-24 16:05:50 -0700

    fixed typo

--- src/Pages/Tldr.tsx
+++ src/Pages/Tldr.tsx
@@ -3,7 +3,7 @@ export default function Tldr(){
     return (
         <div>
         
-        Hi, I'm Adhip! I'm a Full-Stack Software Developer, and recent M.S Computer Science graduate from Arizona State University.<br/>
+        Hi, I'm Adhip! I'm a Full-Stack Software Developer, and recent M.S. Computer Science graduate from Arizona State University.<br/>
         I'm interested in Full stack web Development and AI. Looking for full-time Software developer roles.
         <FindMe/>
 
